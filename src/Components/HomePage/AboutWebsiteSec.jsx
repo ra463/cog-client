aa58c68e46743e426c77544b798d19653c933dd0
ABHINAV ABHI <Abhinav Thonti>
2025-07-19 20:02:01 +0530

    InitialSetup

---
+++ src/Components/HomePage/AboutWebsiteSec.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutWebsiteSec() {
+  return (
+    <div>
+      <p>About</p>
+    </div>
+  )
+}
+
+export default AboutWebsiteSec
+
---
+++ src/Components/HomePage/CardsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardsSection() {
+  return (
+    <div>
+      <p>Cards</p>
+    </div>
+  )
+}
+
+export default CardsSection
