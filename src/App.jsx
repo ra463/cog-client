20bf4d95e3ec4e2f67d3bbdada523da0485cc5f8
Durga-prasad0170 <pamujuladurga@gmail.com>
2025-07-21 21:07:49 +0530

    completed nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Home from "./Components/HomePage/Home"
 function App() {
  
   return (
-    <>
+    <div className="bg-[#003d66] h-lvh w-screen">
       <Home></Home>
       
-    </>
+    </div>
   );
 }
 
